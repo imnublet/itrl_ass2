f559ac383f0558c2c3c5664f384261164a157cfa
Sayf <sayfgmaul@gmail.com>
2022-03-16 14:41:26 +0100

    created helper function for doing repetition and multiple and episodes

--- modelfree_assignment_students/ShortCutAgents.py
+++ modelfree_assignment_students/ShortCutAgents.py
@@ -26,21 +26,23 @@ class QLearningAgent(object):
 
 class SARSAAgent(object):
 
-    def __init__(self, n_actions, n_states, epsilon):
+    def __init__(self, n_actions, actions, n_states, epsilon=0.25, alpha=0.1):
+        self.alpha = alpha
         self.n_actions = n_actions
+        self.actions = actions
         self.n_states = n_states
         self.epsilon = epsilon
-        # TO DO: Add own code
-        pass
-        
+        self.q_table = np.zeros(shape=(self.n_states, self.n_actions))
+
     def select_action(self, state):
-        # TO DO: Add own code
-        a = random.choice(range(self.n_actions)) # Replace this with correct action selection
+        if np.random.random() < self.epsilon:
+            a = np.random.choice(self.actions, size=1)[0]
+        else:
+            a = np.argmax(self.q_table[state])
         return a
-        
-    def update(self, state, action, reward):
-        # TO DO: Add own code
-        pass
+
+    def update(self, state, next_state, action, next_action, reward):
+        self.q_table[state, action] = self.q_table[state, action] + self.alpha * (reward + self.q_table[next_state, next_action] - self.q_table[state, action])
 
 
 class ExpectedSARSAAgent(object):
--- modelfree_assignment_students/ShortCutExperiment.py
+++ modelfree_assignment_students/ShortCutExperiment.py
@@ -2,22 +2,65 @@ from unicodedata import name
 from ShortCutEnvironment import *
 from ShortCutAgents import *
 import numpy as np
+# TODO: Q_table visualiseren voor de environment
 
-
-def run_episodes(n_episodes=10, alpha=0.1, epsilon=0.25):
+def run_episodes(agent_type='q_learning', n_episodes=30, n_reps=50, alpha=0.1, epsilon=0.1):
     """
     """
     env, n_actions, n_states, actions = load_env()
-    for episode in range(n_episodes):
+
+    if agent_type == 'q_learning':
+        return q_learning(env, n_actions, n_states, actions, n_episodes, n_reps, epsilon, alpha)
+    if agent_type == 'sarsa':
+        sarsa(env, n_actions, n_states, actions, n_episodes, n_reps, epsilon, alpha)
+
+    # print(averaged_cumulative_rewards)
+
+
+def q_learning(env, n_actions, n_states, actions, n_episodes, n_reps, epsilon, alpha):
+    """
+
+    """
+    all_cumulative_rewards = np.empty(shape=n_episodes)
+    for rep in range(n_reps):
+        reward_per_episode = np.empty(0)
         agent = QLearningAgent(n_actions, actions, n_states, epsilon, alpha)
+        for episode in range(n_episodes):
+            total_reward = 0
+            while not env.done():
+                state = env.state()
+                action = agent.select_action(state)
+                reward = env.step(action)
+                total_reward += reward
+                new_state = env.state()
+                agent.update(state, new_state, action, reward)
+            reward_per_episode = np.append(reward_per_episode, total_reward)
+            # print('total_reward', total_reward)
+            # # print(reward_per_episode[episode])
+            # # print(np.shape(reward_per_episode))
+            # print('ep', episode)
+            # print('r_per_e', reward_per_episode)
+            env.reset()
+        all_cumulative_rewards = np.vstack((all_cumulative_rewards, reward_per_episode))
+    return np.mean(all_cumulative_rewards, axis=0)
+
+
+def sarsa(env, n_actions, n_states, actions, n_episodes, epsilon, alpha):
+    """
+
+    """
+    for episode in range(n_episodes):
+        agent = SARSAAgent(n_actions, actions, n_states, epsilon, alpha)
         while not env.done():
             state = env.state()
             action = agent.select_action(state)
             reward = env.step(action)
             new_state = env.state()
-            agent.update(state, new_state, action, reward)
+            new_action = agent.select_action(new_state)
+            agent.update(state, new_state, action, new_action, reward)
             print('current state:', state)
         env.reset()
+    pass
 
 
 def load_env():
@@ -31,4 +74,7 @@ def load_env():
 
     return loaded_env, n_actions, n_states, actions
 
-run_episodes()
+
+all_cumulative_rewards = run_episodes(agent_type='q_learning')
+print(all_cumulative_rewards)
+# run_episodes(agent_type='sarsa')
