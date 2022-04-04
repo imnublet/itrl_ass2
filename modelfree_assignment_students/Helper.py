61b407f56f90c2e524f4cea8f6aa8ca06969cad3
Luca <luca@mbp-van-luca.home>
2022-04-04 23:18:01 +0200

    Fixes plot function

--- modelfree_assignment_students/Helper.py
+++ modelfree_assignment_students/Helper.py
@@ -97,10 +97,8 @@ class MatrixPlot:
         for i in range(self.x):
             for j in range(self.y):
                 pos = max_q_table[i, j]
-                if pos != 0:
-                    self.ax.text(i, j, self.action_to_string(grid[i, j]), va='center', ha='center', size=10)
-                else:
-                    self.ax.text(i, j, ' ', va='center', ha='center', size=10)
+                self.ax.text(i, j, np.round(pos,1), va='center', ha='center', size=10)
+                
         self.ax.matshow(max_q_table, cmap=plt.cm.Blues)
         self.ax.axes.xaxis.set_visible(False)
         self.ax.axes.yaxis.set_visible(False)
